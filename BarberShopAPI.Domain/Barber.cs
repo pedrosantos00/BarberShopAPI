87de55d337e8e75b6fdf285edb35c0c66684a583
Pedro Santos <pedrobocas00@hotmail.com>
2023-07-05 22:15:12 +0100

    Add project files.

---
+++ BarberShopAPI.Domain/Barber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberShopAPI.Domain
+{
+    public class Barber
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public List<Reservation> Reservations { get; set; }
+        public List<Availability> Availability { get; set; }
+    }
+}
